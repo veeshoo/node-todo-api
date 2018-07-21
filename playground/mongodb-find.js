989b56da5cacbbc7ea35b84b6ab69d7a15eebca2
veeshoo@gmail.com <veeshoo@gmail.com>
2018-07-21 11:22:45 +0530

    first commit in node todo api

---
+++ playground/mongodb-find.js
@@ -0,0 +1,24 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log("Unable to connect to Mongodb server");
+    }
+    console.log("Connected to Mongodb Server");
+
+    // db.collection('Todos').find({_id: new ObjectId('5b5184cedb5c073a18b82606')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log("Unable to fetch todos", err);
+    // });
+
+    db.collection('Users').find({"location": "Philadelphia"}).count().then((count) => {
+        console.log(`Total count: ${count}`);
+        // console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log("Unable to fetch todos", err);
+    });
+    //db.close();
+});
